51b8330940fc77dfec4907da2c3bcd7da133bf03
David Hallin <hallin.david@gmail.com>
2020-02-24 09:40:22 -0500

    redeployed with full https domain name in vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-    ? '/https://www.bootstrapvueformbuilder.com/'
+    ? 'https://www.bootstrapvueformbuilder.com/'
     : '/'
 }
