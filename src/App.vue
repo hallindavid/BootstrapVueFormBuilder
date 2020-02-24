d14717ff8bf37b17c4bf851a6ca8ba0b399991ea
David Hallin <hallin.david@gmail.com>
2020-02-24 08:44:38 -0500

    Fixed header link to router item

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
      <header>
       <!-- Fixed navbar -->
       <b-navbar toggleable="md" type="dark" variant="dark" fixed="top">
-        <b-navbar-brand href="/">Bootstrap Vue Form Builder</b-navbar-brand>
+        <b-navbar-brand to="/">Bootstrap Vue Form Builder</b-navbar-brand>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
             <b-nav-item to="/" >Home</b-nav-item>
