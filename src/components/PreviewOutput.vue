1ca21f9f44ca98fd1cf1ca356132bdede8319ec8
David Hallin <hallin.david@gmail.com>
2020-03-02 05:18:38 -0500

    Added custom classes on b-rows - only on code output, not on preview because we won't have custom css in this platform

--- src/components/PreviewOutput.vue
+++ src/components/PreviewOutput.vue
@@ -2,7 +2,7 @@
 	<div>
 		<template v-for="(row,row_index) in rows">
 			<b-row :key="row_index" class="mt-2">
-				<template v-for="(element, element_index) in row">
+				<template v-for="(element, element_index) in row.items">
 					<component v-bind:is="get_preview_component(element)" v-bind:element="element" :key="element_index"></component>
 				</template>
 			</b-row>
